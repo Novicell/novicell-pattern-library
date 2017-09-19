758cc593a08dba5fbcd8106ff0d9ef2c3432dc6c
Brian Stefan Jensen <bsj@novicell.dk>
2017-09-19 12:53:56 +0200

    Added navigation

---
+++ patterns/01-modules/navigation/main-menu-link/main-menu-link.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: "Main menu link",
+  status: "prototype",
+  context: {
+    "url": "Link text",
+    "title": "Link title",
+    "text": "Link text"
+  }
+};
---
+++ patterns/01-modules/navigation/main-menu/main-menu.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: "Main menu link",
+  status: "prototype",
+  items: [{
+    "url": "Link 1",
+    "title": "Link 1 title",
+    "text": "Link 1 text"
+  },{
+    "url": "Link 2 text",
+    "title": "Link 2 title",
+    "text": "Link 2 text"
+  }]
+};
